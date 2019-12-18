66a529a3570053ba4211abfe404e8a05e48354be
chenhao <563083573@qq.com>
2019-12-18 11:44:50 +0800

    comment

--- src/main/java/leetcode_29/Answer.java
+++ src/main/java/leetcode_29/Answer.java
@@ -5,12 +5,14 @@ package leetcode_29;
  * @Date: 2019/12/18
  * @Description: 两数相除
  */
-//用移位运算（<<）模拟CPU内部的除法过程，时间复杂度为log(n)，暴力解为n
+//要点1、用移位运算（<<）模拟CPU内部的除法过程，时间复杂度为log(n)，暴力解为n
+//要点2、Int负数比正数多1个，所以正数求反没毛病，负数求反就有很多麻烦事
 public class Answer {
     public int divide(int dividend, int divisor) {
         boolean sign = (dividend > 0) ^ (divisor > 0);
         int result = 0;
         // 因为负数比整数多一个，将整数转化为负数处理
+        // 如果转为整数处理，-2147483648/-1就需要做特殊处理，比较麻烦
         if(dividend > 0) dividend = -dividend;
         if(divisor > 0) divisor = -divisor;
         while(dividend <= divisor) {
