87052eb7b0c8ef5bda3b52445430e2678d07b083
chenhao <563083573@qq.com>
2019-12-19 15:31:46 +0800

    get a lesson

---
+++ src/main/java/leetcode_32/Answer2.java
@@ -0,0 +1,38 @@
+package leetcode_32;
+
+/**
+ * @Auther: Chen Hao
+ * @Date: 2019/12/19
+ * @Description: 最长有效括号(双计数器法)
+ */
+public class Answer2 {
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
+}
