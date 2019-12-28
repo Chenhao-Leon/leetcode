1306fd302d22af576877941fd9010c4ef3db1599
chenhao <563083573@qq.com>
2019-12-28 11:08:30 +0800

    answer

---
+++ src/main/java/leetcode_45/Answer2.java
@@ -0,0 +1,28 @@
+package leetcode_45;
+
+/**
+ * @Auther: Chen Hao
+ * @Date: 2019/12/28
+ * @Description:
+ */
+public class Answer2 {
+    public int jump(int[] nums) {
+        int end = 0;
+        //maxPosition是当i<=index<=end是的最远位置
+        int maxPosition = 0;
+        int steps = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            //找能跳的最远的
+            maxPosition = Math.max(maxPosition, nums[i] + i);
+            if( i == end){ //遇到边界，就更新边界，并且步数加一
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Answer2().jump(new int[]{2,3,1,1,4}));
+    }
+}
