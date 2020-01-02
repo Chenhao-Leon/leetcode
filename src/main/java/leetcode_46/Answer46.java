530acabc2228903d8bd83e5125e24d02614ce5c7
chenhao <563083573@qq.com>
2020-01-02 15:39:11 +0800

    answer

--- src/main/java/leetcode_46/Answer46.java
+++ src/main/java/leetcode_46/Answer46.java
@@ -27,25 +27,19 @@ public class Answer46 {
                                   ArrayList<Integer> nums,
                                   List<List<Integer>> output,
                                   int first) {
-                // if all integers are used up
                 if (first == n)
                     output.add(new ArrayList<Integer>(nums));
                 for (int i = first; i < n; i++) {
-                    // place i-th integer first
-                    // in the current permutation
+                    // 用于交换Collection的元素
                     Collections.swap(nums, first, i);
-                    // use next integers to complete the permutations
                     backtrack(n, nums, output, first + 1);
-                    // backtrack
                     Collections.swap(nums, first, i);
                 }
             }
 
             public List<List<Integer>> permute(int[] nums) {
-                // init output list
                 List<List<Integer>> output = new LinkedList();
 
-                // convert nums into list since the output is a list of lists
                 ArrayList<Integer> nums_lst = new ArrayList<Integer>();
                 for (int num : nums)
                     nums_lst.add(num);
