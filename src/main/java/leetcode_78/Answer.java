963af0dc3a4e815d09a3a79aecd50352842a3542
chenhao <563083573@qq.com>
2020-02-11 12:04:04 +0800

    answer

---
+++ src/main/java/leetcode_78/Answer.java
@@ -0,0 +1,26 @@
+package leetcode_78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: Chen Hao
+ * @Date: 2020/2/10
+ * @Description: 子集
+ */
+class Answer {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res, new ArrayList<Integer>());
+        return res;
+    }
+
+    private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> tmp) {
+        res.add(new ArrayList<>(tmp));
+        for (int j = i; j < nums.length; j++) {
+            tmp.add(nums[j]);
+            backtrack(j + 1, nums, res, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
