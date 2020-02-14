36d70d311e65c3c5bae62b350476527638135262
chenhao <563083573@qq.com>
2020-02-14 11:28:16 +0800

    my solution

--- src/main/java/leetcode_101/Solution101_2.java
+++ src/main/java/leetcode_101/Solution101_2.java
@@ -9,32 +9,54 @@ import java.util.LinkedList;
  */
 public class Solution101_2 {
     public boolean isSymmetric(TreeNode root) {
+        //借鉴答案后更改
         LinkedList<TreeNode> leftToRight = new LinkedList<>();
         LinkedList<TreeNode> rightToLeft = new LinkedList<>();
-        if(root != null) {
-            leftToRight.add(root);
-            rightToLeft.add(root);
-        }
+        leftToRight.add(root);
+        rightToLeft.add(root);
         TreeNode p1 = null;
         TreeNode p2 = null;
         while(!leftToRight.isEmpty() && !rightToLeft.isEmpty()){
             p1 = leftToRight.pollFirst();
             p2 = rightToLeft.pollFirst();
+            if(p1 == null && p2 == null)
+                continue;
+            else if(p1 == null || p2 == null)
+                return false;
             if(p1.val != p2.val)
                 return false;
-            if((p1.left == null && p2.right != null) || (p1.left != null && p2.right == null))
-                return false;
-            if((p1.right == null && p2.left != null) || (p1.right != null && p2.left == null))
-                return false;
-            if(p1.left != null)
-                leftToRight.add(p1.left);
-            if(p1.right != null)
-                leftToRight.add(p1.right);
-            if(p2.right != null)
-                rightToLeft.add(p2.right);
-            if(p2.left != null)
-                rightToLeft.add(p2.left);
+            leftToRight.add(p1.left);
+            leftToRight.add(p1.right);
+            rightToLeft.add(p2.right);
+            rightToLeft.add(p2.left);
         }
         return true;
+//        LinkedList<TreeNode> leftToRight = new LinkedList<>();
+//        LinkedList<TreeNode> rightToLeft = new LinkedList<>();
+//        if(root != null) {
+//            leftToRight.add(root);
+//            rightToLeft.add(root);
+//        }
+//        TreeNode p1 = null;
+//        TreeNode p2 = null;
+//        while(!leftToRight.isEmpty() && !rightToLeft.isEmpty()){
+//            p1 = leftToRight.pollFirst();
+//            p2 = rightToLeft.pollFirst();
+//            if(p1.val != p2.val)
+//                return false;
+//            if((p1.left == null && p2.right != null) || (p1.left != null && p2.right == null))
+//                return false;
+//            if((p1.right == null && p2.left != null) || (p1.right != null && p2.left == null))
+//                return false;
+//            if(p1.left != null)
+//                leftToRight.add(p1.left);
+//            if(p1.right != null)
+//                leftToRight.add(p1.right);
+//            if(p2.right != null)
+//                rightToLeft.add(p2.right);
+//            if(p2.left != null)
+//                rightToLeft.add(p2.left);
+//        }
+//        return true;
     }
 }
