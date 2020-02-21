f3e76cec461c965cde7110a3fe8ef4f15d1bb3b4
chenhao <563083573@qq.com>
2020-02-21 14:43:17 +0800

    Answer

---
+++ src/main/java/leetcode_138/Answer138.java
@@ -0,0 +1,27 @@
+package leetcode_138;
+
+import java.util.HashMap;
+
+/**
+ * @Auther: Chen Hao
+ * @Date: 2020/2/21
+ * @Description:
+ */
+public class Answer138 {
+    HashMap<Node, Node> visitedHash = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (this.visitedHash.containsKey(head)) {
+            return this.visitedHash.get(head);
+        }
+        Node node = new Node(head.val);
+        this.visitedHash.put(head, node);
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+
+        return node;
+    }
+}
