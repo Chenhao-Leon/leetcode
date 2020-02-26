0358bc47f4938cb80d199642e65fe632f250ee6f
chenhao <563083573@qq.com>
2020-02-26 18:46:33 +0800

    merge sort

--- src/main/java/leetcode_152/Answer.java
+++ src/main/java/leetcode_152/Answer.java
@@ -7,19 +7,19 @@ package leetcode_152;
  */
 public class Answer {
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
-        }
-        int dpMax = nums[0];
-        int dpMin = nums[0];
-        int max = nums[0];
-        for (int i = 1; i < n; i++) {
-            int preMax = dpMax;
-            dpMax = Math.max(dpMin * nums[i], Math.max(dpMax * nums[i], nums[i]));
-            dpMin = Math.min(dpMin * nums[i], Math.min(preMax * nums[i], nums[i]));
-            max = Math.max(max, dpMax);
-        }
-        return max;
+            int n = nums.length;
+            if (n == 0) {
+                return 0;
+            }
+            int dpMax = nums[0];
+            int dpMin = nums[0];
+            int max = nums[0];
+            for (int i = 1; i < n; i++) {
+                int preMax = dpMax;
+                dpMax = Math.max(dpMin * nums[i], Math.max(dpMax * nums[i], nums[i]));
+                dpMin = Math.min(dpMin * nums[i], Math.min(preMax * nums[i], nums[i]));
+                max = Math.max(max, dpMax);
+            }
+            return max;
     }
 }
