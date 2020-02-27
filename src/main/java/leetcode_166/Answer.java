2467fae4eb150899c5915632f85e3ea719ca5a7f
chenhao <563083573@qq.com>
2020-02-27 10:49:34 +0800

    Answer

---
+++ src/main/java/leetcode_166/Answer.java
@@ -0,0 +1,44 @@
+package leetcode_166;
+
+import java.util.Map;
+import java.util.HashMap;
+/**
+ * @Auther: Chen Hao
+ * @Date: 2020/2/27
+ * @Description: 分数到小数
+ */
+public class Answer {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder fraction = new StringBuilder();
+        // If either one is negative (not both)。异或用法，学到了
+        if (numerator < 0 ^ denominator < 0) {
+            fraction.append("-");
+        }
+        // Convert to Long or else abs(-2147483648) overflows
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(String.valueOf(dividend / divisor));
+        long remainder = dividend % divisor;
+        if (remainder == 0) {
+            return fraction.toString();
+        }
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            //判断循环的标志是该余数有没有出现过
+            if (map.containsKey(remainder)) {
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
+                break;
+            }
+            map.put(remainder, fraction.length());
+            remainder *= 10;
+            fraction.append(String.valueOf(remainder / divisor));
+            remainder %= divisor;
+        }
+        return fraction.toString();
+    }
+}
