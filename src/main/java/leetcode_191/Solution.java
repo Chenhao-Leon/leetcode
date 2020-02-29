dc90e8f5383a7d55b35ddae7a069e73e7a5181e2
chenhao <563083573@qq.com>
2020-02-29 10:52:09 +0800

    answer

---
+++ src/main/java/leetcode_191/Solution.java
@@ -0,0 +1,18 @@
+package leetcode_191;
+
+/**
+ * @Auther: Chen Hao
+ * @Date: 2020/2/29
+ * @Description: 位1的个数
+ */
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            int tmp = n & 1;
+            if(tmp == 1) count++;
+            n >>= 1;
+        }
+        return count;
+    }
+}
