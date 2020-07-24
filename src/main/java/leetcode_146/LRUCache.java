f5eb7f82a053ea8aa0bf0319e7f9ee3bbed99384
Chenhao-Leon <563083573@qq.com>
2020-07-24 11:52:46 +0800

    LRU

--- src/main/java/leetcode_146/LRUCache.java
+++ src/main/java/leetcode_146/LRUCache.java
@@ -1,57 +1,84 @@
 package leetcode_146;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
 
-/**
- * @Auther: Chen Hao
- * @Date: 2020/2/23
- * @Description: LRU缓存机制 时间10%
- */
 public class LRUCache {
+    class Node {
+        public int key;
+        public int val;
+        public Node next;
+        public Node before;
+
+        public Node (int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
     private int capacity;
-    private Map<Integer, Integer> map = new HashMap<>();
-    private int[] queue;
     private int size = 0;
+    private HashMap<Integer, Node> map;
+    private Node head;
+    private Node tail;
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        queue = new int[capacity];
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.before = head;
     }
 
     public int get(int key) {
-        if(map.containsKey(key)){
-            updateQueue(key);
-            return map.get(key);
+        if(map.containsKey(key)) {
+            Node node = map.get(key);
+            removeKey(key);
+            addNodeToTail(node);
+            //注意：addNodeToTail方法从map中移除了key，因此还要重新加入
+            map.put(key, node);
+            return node.val;
+        } else {
+            return -1;
         }
-        return -1;
     }
 
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-            updateQueue(key);
-            map.put(key, value);
-            return;
-        }
-        if(size == capacity) {
-            map.remove(queue[0]);
-            for (int i = 1; i < size; i++)
-                queue[i - 1] = queue[i];
-            size--;
-        }
-        queue[size++] = key;
-        map.put(key, value);
-    }
-    private void updateQueue(int key){
-        for(int i = 0; i < size; i++){
-            if(queue[i] != key) continue;
-            for(int j = i; j < size - 1; j++){
-                int tmp = queue[j];
-                queue[j] = queue[j + 1];
-                queue[j + 1] = tmp;
+        if(map.containsKey(key)) {
+            //注意这里需要将节点连到尾部
+            Node node = map.get(key);
+            node.val = value;
+            get(key);
+        } else {
+            Node node = new Node(key, value);
+            if(size >= capacity) {
+                removeKey(head.next.key);
+            } else {
+                size++;
             }
-            break;
+            addNodeToTail(node);
+            map.put(key, node);
+        }
+    }
+
+    private void addNodeToTail(Node node) {
+        Node tmp = tail.before;
+        tmp.next = node;
+        node.before = tmp;
+        node.next = tail;
+        tail.before = node;
+    }
+
+    private int removeKey(int key) {
+        if(!map.containsKey(key)){
+            return -1;
+        } else {
+            Node node = map.get(key);
+            Node tmp = node.before;
+            tmp.next = node.next;
+            node.next.before = tmp;
+            map.remove(key);
+            return 1;
         }
     }
 }
--- src/test/java/leetcode_146/LRUCacheTest.java
+++ src/test/java/leetcode_146/LRUCacheTest.java
@@ -2,26 +2,34 @@ package leetcode_146;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class LRUCacheTest {
-    @Test
-    public void testLRUCache(){
-        LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
 
-        cache.put(2, 6);
-        cache.put(1, 5);
-        cache.put(1, 2);
-        assertEquals(2, cache.get(1));
-        assertEquals(6, cache.get(2));
-        cache.put(1, 1);
-        cache.put(2, 2);
-        cache.get(1);       // 返回  1
-        cache.put(3, 3);    // 该操作会使得密钥 2 作废
-        cache.get(2);       // 返回 -1 (未找到)
-        cache.put(4, 4);    // 该操作会使得密钥 1 作废
-        cache.get(1);       // 返回 -1 (未找到)
-        cache.get(3);       // 返回  3
-        cache.get(4);       // 返回  4
+    @Test
+    public void get() {
+//        LRUCache2 cache = new LRUCache2(2);
+//        cache.put(1, 1);
+//        cache.put(2, 2);
+//        System.out.println(cache.get(1));;       // 返回  1
+//        cache.put(3, 3);    // 该操作会使得关键字 2 作废
+//        System.out.println(cache.get(2));;       // 返回 -1 (未找到)
+//        cache.put(4, 4);    // 该操作会使得关键字 1 作废
+//        System.out.println(cache.get(1));;       // 返回 -1 (未找到)
+//        System.out.println(cache.get(3));;       // 返回  3
+//        System.out.println(cache.get(4));;       // 返回  4
+
+//        LRUCache2 cache = new LRUCache2(2);
+//        cache.put(2, 1);
+//        cache.put(2, 2);
+//        System.out.println(cache.get(2));
+//        cache.put(1, 1);
+//        cache.put(4, 4);
+//        System.out.println(cache.get(2));
+
+        LRUCache cache = new LRUCache(1);
+        cache.put(2, 1);
+        System.out.println(cache.get(2));
+        cache.put(3, 2);
+        System.out.println(cache.get(2));
+        System.out.println(cache.get(3));
     }
 }
