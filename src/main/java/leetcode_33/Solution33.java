35bf3ca87cf043b44f00d1035da34a73434a335f
Chenhao-Leon <563083573@qq.com>
2020-07-27 19:22:28 +0800

    solution33

---
+++ src/main/java/leetcode_33/Solution33.java
@@ -0,0 +1,37 @@
+package leetcode_33;
+
+public class Solution33 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while(left <= right) {
+            mid = (left + right) / 2;
+            if(nums[mid] == target)
+                return mid;
+            //[left,right]为升序，正常二分法
+            if(nums[left] < nums[right]) {
+                if(nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            //[left,right]非升序，特殊处理
+            else {
+                //nums[mid]处于前半段
+                if(nums[mid] >= nums[left]) {
+                    if(target >= nums[left] && target < nums[mid])
+                        right = mid - 1;
+                    else left = mid + 1;
+                }
+                //nums[mid]处于后半段
+                else {
+                    if(target <= nums[right] && target > nums[mid])
+                        left = mid + 1;
+                    else right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/leetcode_33/Solution33Test.java
@@ -0,0 +1,13 @@
+package leetcode_33;
+
+import org.junit.Test;
+
+public class Solution33Test {
+
+    @Test
+    public void search() {
+        int[] nums = {4,5,6,7,0,1,2};
+        Solution33 solution33 = new Solution33();
+        System.out.println(solution33.search(nums, 0));
+    }
+}
