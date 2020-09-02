edb7a98d078feab76708be097e5fd9971ebb5782
chenhao <563083573@qq.com>
2020-09-02 15:18:01 +0800

    new solution 25

--- src/main/java/leetcode_25/Solution.java
+++ src/main/java/leetcode_25/Solution.java
@@ -2,42 +2,36 @@ package leetcode_25;
 
 /**
  * @Auther: Chen Hao
- * @Date: 2019/12/16
- * @Description: K 个一组翻转链表
+ * @Date: 2020/9/2
+ * @Description:
  */
 public class Solution {
-    public ListNode reverseKGroup(ListNode head, int k){
-        ListNode start = head;
-        ListNode end = head;
-        //在头结点之前增加空结点，之后处理头结点无需单独处理
-        ListNode beforeHead = new ListNode(0);
-        beforeHead.next = head;
-        ListNode pre = beforeHead;
-        while(start != null){
-            int count = k;
-            while(count != 0 && end != null) {
-                end = end.next;
-                count --;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int len = 0;
+        ListNode p = head;
+        while (p != null) {
+            p = p.next;
+            len++;
+        }
+        ListNode result = new ListNode(0);
+        ListNode tail = result;
+        ListNode start;
+        int nowIndex = 0;
+        p = head;
+        while (nowIndex <= len && len - nowIndex >= k) {
+            start = tail;
+            for (int i = 0; i < k; i++) {
+                ListNode tmp = p.next;
+                p.next = start.next;
+                start.next = p;
+                if (i == 0)
+                    tail = p;
+                nowIndex++;
+                p = tmp;
             }
-            if(count != 0) break;
-            pre.next = reverse(start, end);
-            pre = start;
-            start = start.next;
-            end = start;
         }
-        return beforeHead.next;
-    }
-    //方法完成了head~end-1的翻转，并将翻转后的下一指针指向end
-    private ListNode reverse(ListNode head, ListNode end) {
-        ListNode cur = head;
-        ListNode pre = end;
-        while (cur != null && cur != end) {
-            ListNode next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
+        if (nowIndex < len)
+            tail.next = p;
+        return result.next;
     }
 }
-
